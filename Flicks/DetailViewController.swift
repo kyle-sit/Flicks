8c6702a2adbf1ac1ab4383520631170d535be5d2
Kyle Sit <Kyle@kyles-macbook-pro-4.local>
2017-02-12 22:53:50 -0800

    low to high res detail view photo \

--- Flicks/DetailViewController.swift
+++ Flicks/DetailViewController.swift
@@ -30,12 +30,57 @@ class DetailViewController: UIViewController {
         overviewLabel.text = overview
         overviewLabel.sizeToFit()
         
-        let baseURL = "https://image.tmdb.org/t/p/w500"
+        //let baseURL = "https://image.tmdb.org/t/p/w500"
+        
+        var smallImageRequest = URLRequest(url: URL(string: "https://image.tmdb.org/t/p/w45")!)
+        var largeImageRequest = URLRequest(url: URL(string: "https://image.tmdb.org/t/p/original")!)
         
         if let posterPath = movie["poster_path"] as? String {
-            let imageURL = NSURL(string: baseURL + posterPath)
-            posterImage.setImageWith(imageURL as! URL)
+        //let posterPath = movie["poster_path"] as? String
+            smallImageRequest = URLRequest(url: URL(string: "https://image.tmdb.org/t/p/w45" + posterPath)!)
+            largeImageRequest = URLRequest(url: URL(string: "https://image.tmdb.org/t/p/original" + posterPath)!)
         }
+
+        //let smallImageRequest = URLRequest(url: URL(string: "https://image.tmdb.org/t/p/w45")!)
+        //let largeImageRequest = URLRequest(url: URL(string: "https://image.tmdb.org/t/p/original")!)
+        
+        self.posterImage.setImageWith(
+            smallImageRequest,
+            placeholderImage: nil,
+            
+            success: { (smallImageRequest, smallImageResponse, smallImage) -> Void in
+                
+                // smallImageResponse will be nil if the smallImage is already available
+                // in cache (might want to do something smarter in that case).
+                self.posterImage.alpha = 0.0
+                self.posterImage.image = smallImage;
+                
+                UIView.animate(withDuration: 0.3, animations: { () -> Void in
+                    
+                    self.posterImage.alpha = 1.0
+                    
+                }, completion: { (sucess) -> Void in
+                    
+                    // The AFNetworking ImageView Category only allows one request to be sent at a time
+                    // per ImageView. This code must be in the completion block.
+                    self.posterImage.setImageWith(
+                        largeImageRequest,
+                        placeholderImage: smallImage,
+                        success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
+                            
+                            self.posterImage.image = largeImage;
+                            
+                    },
+                        failure: { (request, response, error) -> Void in
+                            // do something for the failure condition of the large image request
+                            // possibly setting the ImageView's image to a default image
+                    })
+                })
+        },
+            failure: { (request, response, error) -> Void in
+                // do something for the failure condition
+                // possibly try to get the large image
+        })
         
         // Do any additional setup after loading the view.
     }
